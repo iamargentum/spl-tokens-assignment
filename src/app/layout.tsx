78ce93a806ffd67490afd9c98aa65330fc478957
iamargentum <rajat.rj97@gmail.com>
2024-08-19 12:53:05 +0530

    variable icon font

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -20,7 +20,8 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
-        <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@24,400,0,0" />
+        {/* google material icons */}
+        <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
       </head>
       <body className={chakraPetch.className}>{children}</body>
     </html>
