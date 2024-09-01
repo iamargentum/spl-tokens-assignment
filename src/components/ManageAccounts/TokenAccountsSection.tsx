39ab6548fff3124f81be91d0519331946e04a8f1
iamargentum <rajat.rj97@gmail.com>
2024-09-02 03:15:08 +0530

    get token balance

--- src/components/ManageAccounts/TokenAccountsSection.tsx
+++ src/components/ManageAccounts/TokenAccountsSection.tsx
@@ -1,15 +1,19 @@
-import { useCallback, useContext } from "react";
+import { useCallback, useContext, useEffect, useState } from "react";
 import { TokenManagerContext } from "../TokenManager";
 import styles from "./tokenAccountsSection.module.css";
 import { useConnection, useWallet } from "@solana/wallet-adapter-react";
 import { Keypair, PublicKey, SystemProgram, Transaction } from "@solana/web3.js";
 import { createAssociatedTokenAccount, createAssociatedTokenAccountInstruction, createInitializeAccountInstruction, getAccountLenForMint, getAssociatedTokenAddress, getMint, TOKEN_PROGRAM_ID } from "@solana/spl-token";
+import { getShortenedString } from "@/utils/common";
 
 export function TokenAccountsSection() {
+    
+    const [tokenBalance, setTokenBalance] = useState('0');
 
     const tokenAccountsContext = useContext(TokenManagerContext);
 
     const defaultSetTokenAccounts = useCallback(() => {}, []);
+
 
     const token = tokenAccountsContext?.token;
     const accounts = tokenAccountsContext?.tokenAccounts?.value || [];
@@ -43,10 +47,30 @@ export function TokenAccountsSection() {
             preflightCommitment: "confirmed"
         });
 
+        console.log("token account created - ", associatedTokenAddress);
+        
+
         setTokenAccounts((prev: PublicKey[]) => [...prev, associatedTokenAddress]);
 
         return associatedTokenAddress;
     }, [connection, publicKey, sendTransaction, setTokenAccounts, token]);
+
+    console.log("accounts changed - ", accounts);
+
+    const getTokenAccountBalance = useCallback(async () => {
+        if(!token) return;
+
+        const res = await connection.getTokenAccountBalance(
+            token.publicKey,
+            "confirmed"
+        );
+
+        setTokenBalance(res.value.uiAmountString || "0");
+    }, [connection, token]);
+
+    useEffect(() => {
+        if(accounts.length > 0) getTokenAccountBalance();
+    }, [accounts, getTokenAccountBalance]);
 
     return (
         <div className={styles.tokenAccountsContainer}>
@@ -62,7 +86,7 @@ export function TokenAccountsSection() {
                         <th className={styles.balanceDataCell}>
                             <span className={styles.balanceDataCellSpan}>
                                 balance
-                                <span className="material-symbols-outlined">
+                                <span className={`material-symbols-outlined ${styles.refreshButton}`} onClick={getTokenAccountBalance}>
                                     refresh
                                 </span>
                             </span>
@@ -73,7 +97,14 @@ export function TokenAccountsSection() {
                     {
                         accounts.length > 0 ?
                             accounts.map((a, aIndex) => (
-                                <tr key={`account_${aIndex}`}></tr>
+                                <tr key={`account_${aIndex}`}>
+                                    <td>
+                                        {getShortenedString(a.toBase58(), 10)}
+                                    </td>
+                                    <td>
+                                        {tokenBalance}
+                                    </td>
+                                </tr>
                             )) :
                             <tr>
                                 <td colSpan={2} className={styles.noTokenAccountsTextCell}>
