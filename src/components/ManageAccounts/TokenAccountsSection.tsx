3ebe3d19573e89f9f266f36f2e49dc08e7860613
iamargentum <rajat.rj97@gmail.com>
2024-09-01 19:22:00 +0530

    add functionality to create token accounts

--- src/components/ManageAccounts/TokenAccountsSection.tsx
+++ src/components/ManageAccounts/TokenAccountsSection.tsx
@@ -1,14 +1,58 @@
-import { useState } from "react";
+import { useCallback, useContext } from "react";
+import { TokenManagerContext } from "../TokenManager";
 import styles from "./tokenAccountsSection.module.css";
+import { useConnection, useWallet } from "@solana/wallet-adapter-react";
+import { Keypair, PublicKey, SystemProgram, Transaction } from "@solana/web3.js";
+import { createAssociatedTokenAccount, createAssociatedTokenAccountInstruction, createInitializeAccountInstruction, getAccountLenForMint, getAssociatedTokenAddress, getMint, TOKEN_PROGRAM_ID } from "@solana/spl-token";
 
-export function TokenAccountsSection({}) {
-    const [accounts, setAccounts] = useState([]);
+export function TokenAccountsSection() {
+
+    const tokenAccountsContext = useContext(TokenManagerContext);
+
+    const defaultSetTokenAccounts = useCallback(() => {}, []);
+
+    const token = tokenAccountsContext?.token;
+    const accounts = tokenAccountsContext?.tokenAccounts?.value || [];
+    const setTokenAccounts = tokenAccountsContext?.tokenAccounts?.setterFn || defaultSetTokenAccounts;
+
+    const { wallet, sendTransaction } = useWallet();
+    const publicKey = wallet?.adapter?.publicKey;
+
+    const { connection } = useConnection();
+
+    const createTokenAccount = useCallback(async () => {
+        console.log("create token account called - ", token, publicKey);
+
+        if (!token || !publicKey) return;
+
+        const associatedTokenAddress = await getAssociatedTokenAddress(
+            token.publicKey,
+            publicKey
+        );
+
+        const transaction = new Transaction().add(
+            createAssociatedTokenAccountInstruction(
+                publicKey,
+                associatedTokenAddress,
+                publicKey,
+                token.publicKey
+            )
+        );
+
+        await sendTransaction(transaction, connection, {
+            preflightCommitment: "confirmed"
+        });
+
+        setTokenAccounts((prev: PublicKey[]) => [...prev, associatedTokenAddress]);
+
+        return associatedTokenAddress;
+    }, [connection, publicKey, sendTransaction, setTokenAccounts, token]);
 
     return (
         <div className={styles.tokenAccountsContainer}>
             <div className={styles.tokenAccountsTopBar}>
                 <h3>token accounts</h3>
-                <button>create new +</button>
+                <button onClick={createTokenAccount}>create new +</button>
             </div>
 
             <table className={styles.tokenAccountsTable}>
@@ -28,14 +72,14 @@ export function TokenAccountsSection({}) {
                 <tbody>
                     {
                         accounts.length > 0 ?
-                        accounts.map((a, aIndex) => (
-                            <tr key={`account_${aIndex}`}></tr>
-                        )) :
-                        <tr>
-                            <td colSpan={2} className={styles.noTokenAccountsTextCell}>
-                                no token accounts
-                            </td>
-                        </tr>
+                            accounts.map((a, aIndex) => (
+                                <tr key={`account_${aIndex}`}></tr>
+                            )) :
+                            <tr>
+                                <td colSpan={2} className={styles.noTokenAccountsTextCell}>
+                                    no token accounts
+                                </td>
+                            </tr>
                     }
                 </tbody>
             </table>
